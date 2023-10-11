967b6348b00305c45373817f4292367679fd4661
Daanan Dasilva <daanandasilva@gmail.com>
2023-10-11 12:12:55 -0600

    pkg structure created

---
+++ src/main/interfaces/Operable.java
@@ -0,0 +1,5 @@
+package src.main.interfaces;
+
+public interface Operable {
+    double performOperation(double a, double b);
+}
---
+++ src/main/operations/Substraction.java
@@ -0,0 +1,4 @@
+package src.main.operations;
+
+public class Substraction {
+}
